6af1280775d2f0d7c2efe1f7482ff7b2a60ef8bb
6drf21e <nir311@163.com>
2024-10-25 20:20:02 +0800

    feat: integrate Google Analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,16 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxtjs/color-mode', '@nuxtjs/tailwindcss'],
+  modules: [
+    '@nuxt/ui',
+    '@nuxtjs/color-mode',
+    '@nuxtjs/tailwindcss',
+    'nuxt-gtag'
+  ],
   colorMode: {
     classSuffix: ''
+  },
+  gtag: {
+    id: process.env.GTAG_ID || 'G-XXXXXXXXXX'
   }
 })
